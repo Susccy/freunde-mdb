b349b733ce99a3074f3e78da4ca381bf40c1e877
Susccy <susccy@gmail.com>
2021-07-22 12:20:21 +0200

    replaced nuxt scaffold with own sample

---
+++ components/Sample.vue
@@ -0,0 +1,26 @@
+<template>
+  <p>{{ message }}</p>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      message: 'test',
+    }
+  },
+  mounted() {
+    console.log(process.env.nodeEnv)
+
+    // this.$axios
+    //   .$get('/api')
+    //   .then((res) => {
+    //     this.message = res
+    //   })
+    //   .catch(console.error)
+  },
+})
+</script>
+
+<style></style>
