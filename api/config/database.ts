37811989199ec62f87ad0cf6efc33db9f3a23795
Susccy <susccy@gmail.com>
2021-07-26 12:43:00 +0200

    better error handling for db connection

--- api/config/database.ts
+++ api/config/database.ts
@@ -2,8 +2,14 @@ import mongoose from 'mongoose'
 
 class Connection {
   constructor() {
-    const uri = process.env.NUXT_ENV_MONGODB_URI!
-    mongoose.connect(uri, (err) => console.error(err!.message))
+    const uri = process.env.NUXT_ENV_MONGODB_LOCAL_URI!
+    mongoose.connect(uri, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Connected to MongoDB');
+      }
+    })
   }
 }
 
