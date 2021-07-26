6d6cff6cf69fd4799593c82f00bfa0e26a56c912
Susccy <susccy@gmail.com>
2021-07-26 17:20:52 +0200

    update sample test to stop failing

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,9 +1,9 @@
-import { mount } from '@vue/test-utils'
-import NuxtLogo from '@/components/NuxtLogo.vue'
+import { mount } from "@vue/test-utils"
+import index from "@/pages/index.vue"
 
-describe('NuxtLogo', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(NuxtLogo)
+describe("index page", () => {
+  test("is a Vue instance", () => {
+    const wrapper = mount(index, { stubs: ["Sample"] })
     expect(wrapper.vm).toBeTruthy()
   })
 })
