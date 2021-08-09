d52f95edf49543e951079b93445ee0415c091e39
Susccy <susccy@gmail.com>
2021-08-09 14:12:28 +0200

    rework virtual total rating

--- src/server/api/models/movie.model.ts
+++ src/server/api/models/movie.model.ts
@@ -7,7 +7,7 @@ export interface IMovieInput {
     eng?: string
     ger?: string
   }
-  rating: { ch: number; rt: number }
+  rating: { ch: number; rt: number; total?: number }
   dateSeen?: Date
   yearReleased?: number
   length?: number
@@ -19,52 +19,61 @@ export interface IMovieInput {
   series?: string
 }
 
-export interface IMovieDoc extends IMovieInput, Document {
-  rating: IMovieInput["rating"] & { total: number }
-}
+export interface IMovieDoc extends IMovieInput, Document {}
 
-const MovieSchema = new Schema<IMovieDoc>({
-  title: {
-    original: { type: String, required: true, unique: true, index: true },
-    eng: {
-      type: String,
-      index: {
-        unique: true,
-        partialFilterExpression: { "title.eng": { $type: "string" } },
+const MovieSchema = new Schema<IMovieDoc>(
+  {
+    title: {
+      original: { type: String, required: true, unique: true, index: true },
+      eng: {
+        type: String,
+        index: {
+          unique: true,
+          partialFilterExpression: { "title.eng": { $type: "string" } },
+        },
+      },
+      ger: {
+        type: String,
+        index: {
+          unique: true,
+          partialFilterExpression: { "title.ger": { $type: "string" } },
+        },
       },
     },
-    ger: {
-      type: String,
-      index: {
-        unique: true,
-        partialFilterExpression: { "title.ger": { $type: "string" } },
-      },
+    rating: {
+      ch: { type: Number, required: true },
+      rt: { type: Number, required: true },
     },
+    dateSeen: Date,
+    yearReleased: Number,
+    length: Number,
+    genres: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
+    fsk: Number,
+    starring: [String],
+    img: { data: Buffer, contentType: String },
+    reviews: [String],
+    series: String,
   },
-  rating: {
-    ch: { type: Number, required: true },
-    rt: { type: Number, required: true },
-  },
-  dateSeen: Date,
-  yearReleased: Number,
-  length: Number,
-  genres: [{ type: Schema.Types.ObjectId, ref: "Genre" }],
-  fsk: Number,
-  starring: [String],
-  img: { data: Buffer, contentType: String },
-  reviews: [String],
-  series: String,
-})
+  {
+    toObject: { getters: true, virtuals: true },
+    toJSON: { getters: true, virtuals: true },
+  }
+)
 
-MovieSchema.virtual("rating.total", function (this: IMovieDoc) {
-  const { ch, rt } = this.rating
-  // Movies rated prior to the beginning of the FREUNDE Filmliste 2 didn't receive individual ratings, only a total.
-  // If a user only inputs a total rating instead of individual ratings, one of the individuals will be automatically set to -1.
-  // That's why we check if either value is negative here, in which case we simply return the other value, otherwise we calculate the average.
-  return Math.min(ch, rt) < 0
-    ? Math.max(ch, rt)
-    : (this.rating.ch + this.rating.rt) / 2
-})
+MovieSchema.virtual("rating.total")
+  .get(function (this: IMovieDoc) {
+    const { ch, rt } = this.rating
+    // Movies rated prior to the beginning of the FREUNDE Filmliste 2 didn't receive individual ratings, only a total.
+    // If a user only inputs a total rating instead of individual ratings, one of the individuals will be automatically set to -1.
+    // That's why we check if either value is negative here, in which case we simply return the other value, otherwise we calculate the average.
+    return Math.min(ch, rt) < 0
+      ? Math.max(ch, rt)
+      : (this.rating.ch + this.rating.rt) / 2
+  })
+  .set(function (this: IMovieDoc, v: number) {
+    this.rating.ch = -1
+    this.rating.rt = v
+  })
 
 // Try to use the existing model if it's been created before.
 // Used to suppress a mongoose error on nuxt hot reload.
