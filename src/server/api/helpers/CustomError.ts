e60ce64a42c74dcbc186448f4366fa827e8db3c0
Susccy <susccy@gmail.com>
2021-08-09 14:01:26 +0200

    add some error helpers

--- src/server/api/helpers/CustomError.ts
+++ src/server/api/helpers/CustomError.ts
@@ -3,3 +3,39 @@ abstract class HttpError {
     this.status = status
   }
 }
+
+export class BadRequestError extends HttpError {
+  constructor () {
+    super(400)
+  }
+}
+
+export class UnauthorizedError extends HttpError {
+  constructor () {
+    super(401)
+  }
+}
+
+export class ForbiddenError extends HttpError {
+  constructor () {
+    super(403)
+  }
+}
+
+export class NotFoundError extends HttpError {
+  constructor () {
+    super(404)
+  }
+}
+
+export class ConflictError extends HttpError {
+  constructor () {
+    super(409)
+  }
+}
+
+export class InternalServerError extends HttpError {
+  constructor () {
+    super(500)
+  }
+}
