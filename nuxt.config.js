e49799d59e565c6cfaa2ae99fd9ba2d0ef9de42f
Susccy <susccy@gmail.com>
2021-08-10 17:31:39 +0200

    upgrade normalize.css to modern-normalize.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: ["~/assets/styles/normalize", "~/assets/styles/main"],
+  css: ["~/assets/styles/modern-normalize", "~/assets/styles/main"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
