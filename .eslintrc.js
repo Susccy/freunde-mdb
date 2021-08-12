22dbbe9ececeae816f559c4a34fe8b52e74190a6
Susccy <susccy@gmail.com>
2021-08-12 15:41:54 +0200

    style: change vue attributes order

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,24 @@ module.exports = {
   plugins: [],
   rules: {
     "@typescript-eslint/no-unused-vars": "warn",
+    "vue/attributes-order": [
+      "warn",
+      {
+        order: [
+          "DEFINITION",
+          "LIST_RENDERING",
+          "CONDITIONALS",
+          "RENDER_MODIFIERS",
+          "GLOBAL",
+          ["UNIQUE", "SLOT"],
+          "TWO_WAY_BINDING",
+          "OTHER_DIRECTIVES",
+          "EVENTS",
+          "CONTENT",
+          "OTHER_ATTR",
+        ],
+        alphabetical: false,
+      },
+    ],
   },
 }
