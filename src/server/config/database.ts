e49eca5bf9b384a3af797f27f1b56536caf785a0
Susccy <susccy@gmail.com>
2021-09-01 15:45:24 +0200

    fix apparently invalid env var name

--- src/server/config/database.ts
+++ src/server/config/database.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose"
 
 // Change env var depending on deployment.
 export default async () => {
-  await connect(process.env._MONGODB_URI!, {
+  await connect(process.env.PRIVATE__MONGODB_URI!, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
