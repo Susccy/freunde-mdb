83834b603bc04a3055e870d0b3f27cbccc5dbedb
Susccy <susccy@gmail.com>
2021-09-01 13:06:37 +0200

    change env var naming

--- src/server/config/database.ts
+++ src/server/config/database.ts
@@ -2,7 +2,7 @@ import { connect } from "mongoose"
 
 // Change env var depending on deployment.
 export default async () => {
-  await connect(process.env.NUXT_ENV_MONGODB_LOCAL_URI!, {
+  await connect(process.env._MONGODB_URI!, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
