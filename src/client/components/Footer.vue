fb2613d95eeaff233e3e2c533a5f9dbc7e1f8b69
$usccy <susccy@gmail.com>
2021-11-25 05:13:34 +0100

    add tmdb legal notice

--- src/client/components/Footer.vue
+++ src/client/components/Footer.vue
@@ -1,6 +1,14 @@
 <template>
   <footer class="c-footer">
-    <p>Da footah</p>
+    <a href="https://www.themoviedb.org/" target="blank" class="c-footer__tmdb">
+      <figure>
+        <img src="~assets/svg/tmdb_logo_full.svg" alt="The Movie DB Logo" />
+        <figcaption>
+          This product uses the TMDB API but is not endorsed or certified by
+          TMDB.
+        </figcaption>
+      </figure>
+    </a>
   </footer>
 </template>
 
