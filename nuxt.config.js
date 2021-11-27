c627ed2ec23deab816ce1eb26813fbd3c3cbcd34
$usccy <susccy@gmail.com>
2021-11-27 05:12:54 +0100

    install vue-slider-component

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
   css: ["./assets/styles/main"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["~/plugins/vue-slider-component.client.ts"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
