0c2fb491606f73316676a988e4de2581dd55ce35
$usccy <susccy@gmail.com>
2021-12-08 21:56:06 +0100

    fix ssr error

--- src/client/components/StyledVueSlider.vue
+++ src/client/components/StyledVueSlider.vue
@@ -1,15 +1,17 @@
 <template>
-  <VueSlider
-    @change="(value) => $emit('input', value)"
-    v-bind="$attrs"
-    :drag-on-click="true"
-    :contained="true"
-    :process-style="{ background: '#00999b' }"
-    :tooltip-style="{
-      background: '#00999b',
-    }"
-    :dot-options="{ focusStyle: { boxShadow: '0 0 2px 1px #70e0dd' } }"
-  />
+  <ClientOnly>
+    <VueSlider
+      @change="(value) => $emit('input', value)"
+      v-bind="$attrs"
+      :drag-on-click="true"
+      :contained="true"
+      :process-style="{ background: '#00999b' }"
+      :tooltip-style="{
+        background: '#00999b',
+      }"
+      :dot-options="{ focusStyle: { boxShadow: '0 0 2px 1px #70e0dd' } }"
+    />
+  </ClientOnly>
 </template>
 
 <script lang="ts">
