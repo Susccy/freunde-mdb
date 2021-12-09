0b34826df6174bdf02b0b89250b878c3c66cfe85
$usccy <susccy@gmail.com>
2021-12-09 20:27:39 +0100

    mobile layout wip styling

--- src/client/layouts/mobile.vue
+++ src/client/layouts/mobile.vue
@@ -5,8 +5,17 @@
     <Nuxt />
     <Footer />
   </div> -->
-  <div>
-    <h1>Mobile version is WIP. Please switch to desktop view for now.</h1>
+  <div style="position: relative; height: 100vh">
+    <!-- prettier-ignore-attribute -->
+    <div
+      style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%);"
+    >
+      <strong style="font-size: 3rem">
+        <p style="text-align: center">
+          Mobile version is WIP. Please switch to desktop view for now.
+        </p>
+      </strong>
+    </div>
   </div>
 </template>
 
