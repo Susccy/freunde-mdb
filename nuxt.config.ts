e724ef5f758a21ce66f242e4944f61c2c975a807
$usccy <susccy@gmail.com>
2022-01-15 12:16:40 +0100

    add global meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,19 +3,23 @@ import type { NuxtConfig } from "@nuxt/types"
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  // head: {
-  //   title: "freunde-mdb",
-  //   htmlAttrs: {
-  //     lang: "en",
-  //   },
-  //   meta: [
-  //     { charset: "utf-8" },
-  //     { name: "viewport", content: "width=device-width, initial-scale=1" },
-  //     { hid: "description", name: "description", content: "" },
-  //     { name: "format-detection", content: "telephone=no" },
-  //   ],
-  //   link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  // },
+  head: {
+    title: "FREundE MDB",
+    htmlAttrs: {
+      lang: "en",
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "FREundE Movie Database - die Filmdatenbank Ihres Vertrauens.",
+      },
+      // { name: "format-detection", content: "telephone=no" },
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+  },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ["~/assets/styles/main"],
