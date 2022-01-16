00e3fa43a707a01c5e8650fd13cf374553138a16
$usccy <susccy@gmail.com>
2022-01-16 12:30:28 +0100

    remove unused head tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default {
       lang: "en",
     },
     meta: [
-      { charset: "utf-8" },
+      // { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
         hid: "description",
@@ -18,7 +18,7 @@ export default {
       },
       // { name: "format-detection", content: "telephone=no" },
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    // link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
